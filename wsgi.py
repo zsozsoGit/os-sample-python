e9eb079b9b24db08dbb6825185138229672dbce9
Zsolt Horváth <zsozsoGit@users.noreply.github.com>
2018-09-24 15:52:53 +0200

    Working?

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,30 @@
-from flask import Flask, redirect, url_for, request, send_from_directory
+from flask import Flask, redirect, url_for, request
+
 app = Flask(__name__, static_url_path='')
+with open('static/sqltest.html', 'r') as content_file:
+    content = content_file.read()
+
 
 @app.route('/')
 def root():
-    return app.send_static_file('/sqltest.html')
-    #return 'welcome TEST'
+    return content
+
+
 
 @app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+    return 'welcome %s' % name
 
-@app.route('/sqltestres',methods = ['POST', 'GET'])
+
+@app.route('/sqltestres', methods=['POST', 'GET'])
 def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
